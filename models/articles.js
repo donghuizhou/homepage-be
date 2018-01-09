40a65ad2d7aace86de6864c55e5ef39243b46ba9
donghuizhou <765966797@qq.com>
2018-01-09 18:18:02 +0800

    增加预览文章功能

--- models/articles.js
+++ models/articles.js
@@ -2,7 +2,6 @@ let mongoose = require('mongoose');
 
 // 定义模型
 let articleSchema = new mongoose.Schema({
-  "articleId": Number,
   "author": String,
   "title": String,
   "articlePath": String,
--- routes/article.js
+++ routes/article.js
@@ -13,54 +13,6 @@ let resCb = (res, code, msg, result) => {
   });
 };
 
-// demo 创建文件并写入数据 & 读取文件内容返回
-router.post('/uploadText', (req, res, next) => {
-  let text = req.body.text;
-  let filename = `text${Date.parse(new Date()) / 1000}`;
-  let filePath = `${process.cwd()}/public/articles/${filename}.txt`;
-  fs.writeFile(filePath, text, {
-    encoding: 'utf-8'
-  }, err => {
-    if (err) throw err;
-    fs.readFile(filePath, (err, data) => {
-      if (err) throw err;
-      resCb(res, 200, 'success', `你刚写入的文件内容是${data}`);
-    });
-  })
-});
-
-// demo 上传图片
-router.post('/uploadImg', (req, res, next) => {
-  // 创建上传表单
-  let form = new formidable.IncomingForm();
-  form.encoding = 'utf-8';
-  // 临时目录
-  form.uploadDir = `${process.cwd()}/public/tmp/`;
-  // 保留后缀
-  form.keepExtensions = true;
-  // 文件大小
-  form.maxFieldsSize = 2 * 1024 * 1024;
-  form.parse(req, (err, fields, files) => {
-    if (err) throw err;
-    // 校验图片格式
-    if (!/^(image\/(p?)jpeg)|(image\/((x-)?)png)$/g.test(files.image.type)) {
-      resCb(res, 501, '只支持png或jpg格式图片');
-    } else {
-      let timestamp = Date.parse(new Date()) / 1000
-      let imgName = `img${timestamp}${files.image.name.split('.')[1]}`;
-      // 图片在服务器完整地址
-      let showPath = `${process.cwd()}/public/imgs/${imgName}`;
-      fs.rename(files.image.path, showPath, (err) => {
-        if (err) throw err;
-        let result = {
-          imgUrl: `http://192.168.31.121:3000/imgs/${imgName}`
-        };
-        resCb(res, 200, '上传成功', result);
-      })
-    }
-  });
-});
-
 // 获取文章列表
 router.get('/getArticles', (req, res, next) => {
   let offset = parseInt(req.query.offset),
@@ -144,21 +96,20 @@ router.post('/createArticle', (req, res, next) => {
   let mdContent = req.body.mdContent,
     radomNum = parseInt(Math.random() * 9000 + 1000),
     createTime = Date.parse(new Date) / 1000,
-    articleId = parseInt(`${radomNum}${createTime}`),
+    fileName = parseInt(`${radomNum}${createTime}`),
     // 文件保存路径
-    filePath = `${process.cwd()}/public/articles/${articleId}.md`;
+    filePath = `${process.cwd()}/public/articles/${fileName}.md`;
     data = {
       author: req.body.author,
       title: req.body.title,
-      articlePath: `/artilces/${articleId}.md`,
+      articlePath: `/artilces/${fileName}.md`,
       createTime,
       tags: req.body.tags,
       category: req.body.cate,
       status: req.body.status,
       del: 0,
       readingAmounts: 0,
-      comments: [],
-      articleId
+      comments: []
     };
   // 写入文件
   fs.writeFile(filePath, mdContent, { encoding: 'utf-8' }, err => {
@@ -166,6 +117,58 @@ router.post('/createArticle', (req, res, next) => {
     Articles.create(data, (err, doc) => {
       if (err) throw err;
       resCb(res, 200, '保存成功');
+    });
+  });
+});
+
+// 图片上传
+router.post('/uploadImg', (req, res, next) => {
+  // 创建上传表单
+  let form = new formidable.IncomingForm();
+  form.encoding = 'utf-8';
+  // 临时目录
+  form.uploadDir = `${process.cwd()}/public/tmp/`;
+  // 保留后缀
+  form.keepExtensions = true;
+  // 文件大小
+  form.maxFieldsSize = 2 * 1024 * 1024;
+  form.parse(req, (err, fields, files) => {
+    if (err) throw err;
+    // 校验图片格式
+    if (!/^(image\/(p?)jpeg)|(image\/((x-)?)png)$/g.test(files.image.type)) {
+      resCb(res, 501, '只支持png或jpg格式图片');
+    } else {
+      let timestamp = Date.parse(new Date()) / 1000;
+      let imgName = `img${timestamp}.${files.image.name.split('.')[1]}`;
+      // 图片在服务器完整地址
+      let showPath = `${process.cwd()}/public/imgs/${imgName}`;
+      fs.rename(files.image.path, showPath, (err) => {
+        if (err) throw err;
+        let result = {
+          imgUrl: `/imgs/${imgName}`
+        };
+        resCb(res, 200, '上传成功', result);
+      });
+    }
+  });
+});
+
+// 根据文章id获取文章
+router.get('/previewArticle', (req, res, next) => {
+  let id = req.query.id
+  Articles.find({'_id': id}, (err, doc) => {
+    if (err) throw err;
+    let filePath = `${process.cwd()}/public${doc[0].articlePath}`;
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) throw err;
+      res.json({
+        code: 200,
+        msg: 'success',
+        result: {
+          content: data,
+          article: doc[0]
+        }
+      })
     });
   });
 });
